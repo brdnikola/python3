a57944aec3e241fcb48d30c905f7085f1467a0e1
brdnikola <116485994+brdnikola@users.noreply.github.com>
2022-11-30 07:57:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Python edited version!"
+    return "Hello Python edited flask version!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
